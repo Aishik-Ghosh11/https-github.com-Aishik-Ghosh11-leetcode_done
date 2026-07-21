0d3c9be7d23969380ed94cb1dfb273ad840f8b68
Aishik Ghosh <174571954+Aishik-Ghosh11@users.noreply.github.com>
2026-07-21 21:21:20 +0530

    Time: 827 ms (40.19%), Space: 21.2 MB (55.14%) - LeetHub

--- 3499-maximize-active-section-with-trade-i/3499-maximize-active-section-with-trade-i.py
+++ 3499-maximize-active-section-with-trade-i/3499-maximize-active-section-with-trade-i.py
@@ -1,43 +1,34 @@
 class Solution:
     def maxActiveSectionsAfterTrade(self, s: str) -> int:
-        ones = s.count('1')
-
-        s = '1' + s + '1'
-        
-        n = len(s)
-        i = 0
-
-        ans = ones
-
-        # Skip first 1's 
-        while i < n and s[i] == '1':
-            i += 1
-        # Read first 0-block
-        c10 = 0
-        while i < n and s[i] == '0':
-            c10 += 1
-            i += 1
-        
-        while i < n:
-            # Read middle 1-Block
-            c11 = 0
-            while i < n and s[i] == '1':
-                c11 += 1
-                i += 1
-            if c11 == 0:
-                break
-            
-            # Read right 0-Block
-            c20 = 0
-            while i < n and s[i] == '0':
-                c20 += 1
-                i += 1
-            
-            if c20 == 0:
-                break
-            ans = max(ans, ones + c10 + c20)
-
-            # Slide the window
-            c10 = c20
-        return ans
-
+        if not s or '1' not in s: return 0
+        if len(s) == 1: return 1 if s == '1' else 0
+        s = s + '1'
+        cur = '1'
+        arr = [1]
+        for x in s:
+            if x == cur == '1':
+                arr[-1]+=1
+            elif x == cur == '0':
+                arr[-1]+=1 #arr[-1]-=1
+            elif x == '0':
+                arr.append(1)
+                cur = x
+            else:
+                arr.append(1)
+                cur=x
+        if len(arr) < 5: return s[:-1].count('1')
+        l, r = 0, 4
+        arr[0]-=1; arr[-1]-=1
+        cur_max=total_sum=sum(arr[::2]); best_r = 1, 3
+        # print(arr)
+        while r < len(arr):
+            # temp_sum = total_sum-sum(arr[l:r+1])+ 2*(arr[l+1] + arr[r-1])
+            temp_sum = total_sum + (arr[l+1] + arr[r-1])
+            if temp_sum >= cur_max:
+                cur_max = temp_sum
+                best_l, best_r = l + 1, r - 1
+            l+=2
+            r+=2
+        # for i in range(len(arr)):
+        # print(f"cur_max: {cur_max}, best_l: {best_l}, best_r: {best_r}")
+        return total_sum + arr[best_l] + arr[best_r]
