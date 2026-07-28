ebb0d8b79c9b10fa7776165b7f074aa372ce4964
Aishik Ghosh <174571954+Aishik-Ghosh11@users.noreply.github.com>
2026-07-28 11:14:02 +0530

    Time: 48 ms (5.55%), Space: 19.7 MB (70.09%) - LeetHub

--- 1582-special-positions-in-a-binary-matrix/1582-special-positions-in-a-binary-matrix.py
+++ 1582-special-positions-in-a-binary-matrix/1582-special-positions-in-a-binary-matrix.py
@@ -1,25 +1,26 @@
 class Solution:
     def numSpecial(self, mat: List[List[int]]) -> int:
-        def check1(row):
-            ans = []
-            for i, x in enumerate(row):
-                if x==1:
-                    ans.append(i)            
-            return ans
-        m=len(mat)
-        n=len(mat[0])
-        ans=0
-        col=[False]*n
-        for row in mat:
-            idx = check1(row)
-            if len(idx) == 1 and not col[j:=idx[0]]:
-                col[j]=True
-                count_col1 = 0
-                for k in range(m):
-                    count_col1 += mat[k][j]
-                if count_col1==1:
-                    ans+=1
-            else:
-                for j in idx:
-                    col[j]==True
-        return ans
+        num_spec = 0
+        lm = len(mat)
+        lr = len(mat[0])
+        for row in range(lm):
+            for col in range(lr):
+                if mat[row][col]:
+                    print(f"row: {row}, col: {col}")
+                    print(f"any1: {any(mat[row][v] for v in range(lr) if v != col)}")
+                    print(f"any2: {any(mat[i][col] for i in range(lm) if i != row)}")
+                    if not any(mat[row][v] for v in range(lr) if v != col) and \
+                    not any(mat[i][col] for i in range(lm) if i != row):
+                        num_spec += 1
+        return num_spec
+
+
+
+
+
+
+
+
+
+
+
