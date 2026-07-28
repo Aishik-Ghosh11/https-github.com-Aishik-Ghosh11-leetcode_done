e65e956e203339bdf46fc1814dac7811547ff2e5
Aishik Ghosh <174571954+Aishik-Ghosh11@users.noreply.github.com>
2026-07-28 09:31:27 +0530

    Time: 794 ms (5.53%), Space: 21.8 MB (12.44%) - LeetHub

--- 3517-smallest-palindromic-rearrangement-i/3517-smallest-palindromic-rearrangement-i.py
+++ 3517-smallest-palindromic-rearrangement-i/3517-smallest-palindromic-rearrangement-i.py
@@ -1,12 +1,53 @@
 class Solution:
     def smallestPalindrome(self, s: str) -> str:
         n = len(s)
-        mid = n//2
+        frequency = [0 for _ in range(26)]
+        for char in s:
+            frequency[ord(char) - ord('a')] += 1
+        
+        output = ['' for _ in range(n)]
+        if n % 2 == 1:
+            for i in range(26):
+                if frequency[i] % 2 == 1:
+                    output[n // 2] = chr(ord('a') + i)
+                    frequency[i] -= 1
+                    break
+        
+        pointer = 0
+        for i in range(26):
+            while frequency[i] > 0:
+                output[pointer] = chr(ord('a') + i)
+                output[n - pointer - 1] = chr(ord('a') + i)
+                pointer += 1
+                frequency[i] -= 2
+        
+        return ''.join(output)
 
-        s_list = list(s)
-        s_list[:mid] = sorted(s_list[:mid])
-        
-        for i in range(mid):
-            s_list[n - 1 - i] = s_list[i]
-        
-        return "".join(s_list)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
