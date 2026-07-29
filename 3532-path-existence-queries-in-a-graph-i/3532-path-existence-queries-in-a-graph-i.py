c728d6573300b954e522599c1129f2ba575141a5
Aishik Ghosh <174571954+Aishik-Ghosh11@users.noreply.github.com>
2026-07-29 13:00:38 +0530

    Time: 359 ms (6.56%), Space: 55.8 MB (13.05%) - LeetHub

--- 3532-path-existence-queries-in-a-graph-i/3532-path-existence-queries-in-a-graph-i.py
+++ 3532-path-existence-queries-in-a-graph-i/3532-path-existence-queries-in-a-graph-i.py
@@ -1,9 +1,19 @@
 class Solution:
     def pathExistenceQueries(self, n: int, nums: List[int], maxDiff: int, queries: List[List[int]]) -> List[bool]:
-        qz, prev, cid=len(queries), -1, 0
-        comp=[-1]*n
-        for i, curr in enumerate(nums):
-            cid+=(prev+maxDiff<curr)
-            comp[i]=cid
-            prev=curr
-        return [ comp[x]==comp[y] for x, y in queries]
+        num_to_min = {}
+        section = nums[0]
+        for i in range(0, len(nums)):
+            if i > 0 and nums[i] > nums[i-1] + maxDiff:
+                section = nums[i]
+            num_to_min[nums[i]] = section
+        print(num_to_min)
+        res = []
+        for query in queries:
+            num0, num1 = nums[query[0]], nums[query[1]]
+            print(num0, num1, num_to_min[num0], num_to_min[num1])
+            if num_to_min[num0] == num_to_min[num1]:
+                res.append(True)
+            else:
+                res.append(False)
+        
+        return res
