92ca4960d54d2a4ab39e803c27be573fd0ab7540
Aishik Ghosh <174571954+Aishik-Ghosh11@users.noreply.github.com>
2026-07-31 20:13:02 +0530

    Time: 23 ms (6.22%), Space: 19.1 MB (98.57%) - LeetHub

--- 1758-minimum-changes-to-make-alternating-binary-string/1758-minimum-changes-to-make-alternating-binary-string.py
+++ 1758-minimum-changes-to-make-alternating-binary-string/1758-minimum-changes-to-make-alternating-binary-string.py
@@ -1,24 +1,7 @@
 class Solution:
     def minOperations(self, s: str) -> int:
-        """
-        :type s: str
-        :rtype: int
-        """
-        ans1 = 0
-        t = s[0]
-        for i in range(1, len(s)):
-            if s[i] == t:
-                ans1 += 1
-                t = str(1 - int(s[i]))
-            else:
-                t = s[i]
-        ans2 = 1
-        t = str(1 - int(s[0]))
-        for i in range(1, len(s)):
-            if s[i] == t:
-                ans2 += 1
-                t = str(1 - int(s[i]))
-            else:                
-                t = s[i]
-        return min(ans1, ans2)
-            
+        st0, st1 = 0,0
+        for i in range(len(s)):
+            st0 += (s[i] == str(i % 2))
+            st1 += (s[i] == str((i + 1) % 2))
+        return min(st0, st1)
