63df39beec9744b09f2d94a0f1573287f887ecca
Aishik Ghosh <174571954+Aishik-Ghosh11@users.noreply.github.com>
2026-08-07 10:13:22 +0530

    Time: 1 ms (29.08%), Space: 19.3 MB (73.71%) - LeetHub

--- 3345-smallest-divisible-digit-product-i/3345-smallest-divisible-digit-product-i.py
+++ 3345-smallest-divisible-digit-product-i/3345-smallest-divisible-digit-product-i.py
@@ -1,19 +1,15 @@
 class Solution:
-    def findDigitsProd(self, num: int) -> int:
-        prod = 1
-        while num > 0:
-            prod = prod * (num % 10)
-            # Early exit if product hits 0
-            if prod == 0:
-                return 0
-            num //= 10 
-        
-        return prod 
-        
     def smallestNumber(self, n: int, t: int) -> int:
-
-        for num in range(n, n + 10):
-            if self.findDigitsProd(num) % t == 0:
-                return num
-
-        return -1
+        # prod = 1
+        while n:
+            prod=1
+            i=n
+            while i:
+                rem = i%10
+                prod*=rem
+                i//=10
+            if prod%t == 0:
+                return n
+            else:
+                n += 1
+        return n
