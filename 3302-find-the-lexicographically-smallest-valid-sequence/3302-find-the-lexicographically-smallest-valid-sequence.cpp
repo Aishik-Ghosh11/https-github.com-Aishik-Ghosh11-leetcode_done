91ec9a10d03e3f6cc00b780fea8307d8b44973bc
Aishik Ghosh <174571954+Aishik-Ghosh11@users.noreply.github.com>
2026-08-09 13:38:02 +0530

    Time: 52 ms (30.85%), Space: 121.2 MB (13.83%) - LeetHub

--- 3302-find-the-lexicographically-smallest-valid-sequence/3302-find-the-lexicographically-smallest-valid-sequence.cpp
+++ 3302-find-the-lexicographically-smallest-valid-sequence/3302-find-the-lexicographically-smallest-valid-sequence.cpp
@@ -1,47 +1,34 @@
 class Solution {
 public:
-    vector<int> validSequence(string word1, string word2) {
-        int n = word1.length();
-        int m = word2.length();
-        
-        // Step 1: Precompute the longest suffix match 
-        // rightHandSideLength[i] will store the maximum length of a suffix 
-        // of word2 that can be found as a subsequence in word1[i...n-1]
-        vector<int> rightHandSideLength(n + 1, 0);
-        int j = m - 1;
-        for (int i = n - 1; i >= 0; --i) {
-            if (j >= 0 && word1[i] == word2[j]) {
-                rightHandSideLength[i] = rightHandSideLength[i + 1] + 1;
+    vector<int> validSequence(string s, string t) {
+        int n=s.size() ,m=t.size();
+
+        vector<int> dp(n+1), ans;
+
+        int j=m-1;
+        bool ok = 1;
+
+        for(int i=n-1; i>=0 ;i--) {
+            dp[i] = dp[i+1];
+
+            if(j>=0&&s[i]==t[j]) {
                 j--;
-            } else {
-                rightHandSideLength[i] = rightHandSideLength[i + 1];
+                dp[i]++;
             }
         }
-        
-        // Step 2: Greedy approach to find the lexicographically smallest indices
-        vector<int> seq;
-        bool changePower = true; // can change only one character
-        j = 0;
-        
-        for (int i = 0; i < n && j < m; ++i) {
-            if (word1[i] == word2[j]) {
-                seq.push_back(i);
+
+        j=0;
+
+        for(int i=0; i<n&&j<m;i++){
+            if(s[i]==t[j]) {
+                ans.push_back(i);
                 j++;
-            } 
-            else if (changePower && rightHandSideLength[i + 1] >= m - j - 1) {
-                // We use our changePower here because picking an earlier index 'i' 
-                // guarantees a lexicographically smaller sequence.
-                seq.push_back(i);
+            } else if(ok&&dp[i+1]>=m-j-1){
+                ans.push_back(i);
+                ok=0;
                 j++;
-                changePower = false;
             }
         }
-        
-        // If we successfully matched all characters of word2, return the sequence
-        if (seq.size() == m) {
-            return seq;
-        }
-        
-        return {};
+        return ans.size()==m?ans:vector<int>{};
     }
 };
