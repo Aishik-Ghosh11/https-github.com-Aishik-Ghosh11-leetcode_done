3459c7cd2c2e44485554cd5bb5971647965de438
Aishik Ghosh <174571954+Aishik-Ghosh11@users.noreply.github.com>
2026-08-09 18:00:29 +0530

    Time: 669 ms (27.66%), Space: 63.9 MB (10.64%) - LeetHub

--- 3302-find-the-lexicographically-smallest-valid-sequence/3302-find-the-lexicographically-smallest-valid-sequence.py
+++ 3302-find-the-lexicographically-smallest-valid-sequence/3302-find-the-lexicographically-smallest-valid-sequence.py
@@ -1,40 +1,18 @@
-from typing import List
-
 class Solution:
     def validSequence(self, word1: str, word2: str) -> List[int]:
-        n = len(word1)
-        m = len(word2)
-
-        last = [-1] * m
-
-        i = n - 1
-        j = m - 1
-
-        while i >= 0 and j >= 0:
-            if word1[i] == word2[j]:
-                last[j] = i
+        n = len(word2)
+        last = [-1]*n
+        j = n-1
+        for i, ch in reversed(list(enumerate(word1))): 
+            if j >= 0 and ch == word2[j]: 
+                last[j] = i 
                 j -= 1
-
-            i -= 1
-
+        j = cnt = 0 
         ans = []
-        can_skip = True
-        j = 0
-
-        for i in range(n):
-            if j == m:
-                break
-
-            if word1[i] == word2[j]:
-                ans.append(i)
-                j += 1
-
-            elif can_skip and (j == m - 1 or i < last[j + 1]):
-                can_skip = False
-                ans.append(i)
-                j += 1
-
-        if j == m:
-            return ans
-
-        return []
+        for i, ch in enumerate(word1): 
+            if j < n: 
+                if ch == word2[j] or cnt == 0 and (j == n-1 or i+1 <= last[j+1]): 
+                    if ch != word2[j]: cnt = 1
+                    ans.append(i)
+                    j += 1
+        return ans if j == n else []
