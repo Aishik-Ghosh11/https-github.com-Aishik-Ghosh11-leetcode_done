b4d0950b61feaf29fb562a0d4c71812b8ed9f197
Aishik Ghosh <174571954+Aishik-Ghosh11@users.noreply.github.com>
2026-08-18 20:09:09 +0530

    Time: 9 ms (26.32%), Space: 32.6 MB (19.17%) - LeetHub

--- 3471-find-the-largest-almost-missing-integer/3471-find-the-largest-almost-missing-integer.cpp
+++ 3471-find-the-largest-almost-missing-integer/3471-find-the-largest-almost-missing-integer.cpp
@@ -1,15 +1,25 @@
 class Solution {
 public:
-    int largestInteger(vector<int>& A, int k) {
-        int f[51] = {0};
-        for (auto& x : A)
-            f[x]++;
-     
-        int res = -1, n = A.size();
-        for (int i = 0; i < n; i++) {
-            if (k == n || (f[A[i]]==1 && (k==1||!i||i==n-1)))
-                res = max(res, A[i]);
+    int largestInteger(vector<int>& nums, int k) {
+        int n = nums.size();
+        unordered_map<int, int> subarray_count;
+        
+        // Process all subarrays of size k
+        for (int i = 0; i <= n - k; ++i) {
+            unordered_set<int> unique_nums(nums.begin() + i, nums.begin() + i + k);
+            for (int num : unique_nums) {
+                subarray_count[num]++;
+            }
         }
-        return res;
+        
+        // Find the largest integer that appears in exactly one subarray
+        int ans = -1;
+        for (const auto& [num, count] : subarray_count) {
+            if (count == 1 && num > ans) {
+                ans = num;
+            }
+        }
+        
+        return ans;
     }
 };
