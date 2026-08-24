bd5b5c85a3ef8d6a3f2a23fbcf96286619e6f05f
Aishik Ghosh <174571954+Aishik-Ghosh11@users.noreply.github.com>
2026-08-24 12:36:26 +0530

    Time: 6 ms (32.39%), Space: 24.9 MB (32.13%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.cpp
+++ 0036-valid-sudoku/0036-valid-sudoku.cpp
@@ -1,18 +1,41 @@
 class Solution {
 public:
-    bool isValidSudoku(vector<vector<char>>& board) {
-        bool rows[9][9] = {false};
-        bool cols[9][9] = {false};
-        bool boxes[9][9] = {false};
-
-        for (int i = 0; i < 9; i++) {
-            for (int j = 0; j < 9; j++) {
-                if (board[i][j] != '.') {
-                    int num = board[i][j] - '1';
-                    int boxIndex = (i / 3) * 3 + (j / 3);
-                    if (rows[i][num] || cols[j][num] || boxes[boxIndex][num]) return false;
-                    rows[i][num] = cols[j][num] = boxes[boxIndex][num] = true;
+    bool isValid3x3( vector<vector<char>>& board, int l, int r ){
+        unordered_set<char> st;
+        for(int i=l; i<l+3; i++){
+            for(int j=r; j<r+3; j++){
+                if( board[i][j] != '.' && st.find(board[i][j]) != st.end() ){
+                    cout << "3 I returned here i : " << i << " j: " << j << endl;
+                    cout << board[i][j];
+                    return false;
                 }
+                st.insert( board[i][j] );
+            }
+        }
+        return true;
+    }
+    bool isValidSudoku(vector<vector<char>>& board) {
+        for(int i=0; i<9; i++){
+            unordered_set<char> hst;
+            unordered_set<char> st;
+            for(int j=0; j<9; j++){
+                if( board[i][j] != '.' && st.find(board[i][j]) != st.end() ){
+                    cout << "1 I returned here i : " << i << " j: " << j << endl;
+                    cout << board[i][j];
+                    return false;
+                }
+                if( board[j][i] != '.' && hst.find(board[j][i]) != hst.end() ){
+                    cout << "2 I returned here i : " << i << " j: " << j << endl;
+                    cout << board[j][i];
+                    return false;
+                }
+                hst.insert(board[j][i]);
+                st.insert(board[i][j]);
+            }
+        }
+        for(int i=0; i<9; i+=3){
+            for(int j=0; j<9; j+=3){
+                if(!isValid3x3(board, i, j)) return false;
             }
         }
         return true;
