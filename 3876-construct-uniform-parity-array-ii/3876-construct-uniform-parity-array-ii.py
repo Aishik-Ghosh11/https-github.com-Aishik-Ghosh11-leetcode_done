2cb06e195f4db2b978d1e04123d97c6712168113
Aishik Ghosh <174571954+Aishik-Ghosh11@users.noreply.github.com>
2026-09-09 20:08:13 +0530

    Time: 141 ms (5.75%), Space: 35.5 MB (19.46%) - LeetHub

--- 3876-construct-uniform-parity-array-ii/3876-construct-uniform-parity-array-ii.py
+++ 3876-construct-uniform-parity-array-ii/3876-construct-uniform-parity-array-ii.py
@@ -1,3 +1,25 @@
 class Solution:
     def uniformArray(self, A: list[int]) -> bool:
-        return not (min(A) ^ reduce(or_, A)) & 1
+        A.sort()
+        n = len(A)
+        x = A[0]
+        if x % 2 == 0:
+            parity = 'even'
+        else:
+            parity = 'odd'
+        for i in range(1, n):
+            t = A[i]
+            if t % 2 == 0:
+                p = 'evev'
+            else: 
+                p = 'odd'
+            
+            if parity != p:
+                s = abs(t - x)
+                if s % 2 == 0:
+                    p = 'even'
+                else: p = 'odd'
+
+                if parity != p:
+                    return False
+        return True
