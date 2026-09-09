01e056a37c711e1ca4af2cb13a96b6b401da17db
Aishik Ghosh <174571954+Aishik-Ghosh11@users.noreply.github.com>
2026-09-09 20:22:59 +0530

    Time: 550 ms (5.53%), Space: 69 MB (25.78%) - LeetHub

--- 2948-make-lexicographically-smallest-array-by-swapping-elements/2948-make-lexicographically-smallest-array-by-swapping-elements.py
+++ 2948-make-lexicographically-smallest-array-by-swapping-elements/2948-make-lexicographically-smallest-array-by-swapping-elements.py
@@ -1,17 +1,55 @@
 class Solution:
-    def lexicographicallySmallestArray(self, A: list[int], limit: int) -> list[int]:
-        groups = []
-        gmap = {}
+    def lexicographicallySmallestArray(self, nums: List[int], limit: int) -> List[int]:
+        n = len(nums)
+        arr = [[nums[i], i] for i in range(n)]
+        arr.sort(key=lambda x:x[0])
 
-        for val in sorted(A):
-            if not groups or val - groups[-1][-1] > limit:
-                groups.append([])
-            groups[-1].append(val)
-            gmap[val] = len(groups) - 1
+        uf = UnionFind(n)
+        for i in range(n-1):
+            if arr[i+1][0] - arr[i][0] <= limit :
+                uf.union(arr[i+1][1], arr[i][1])
+        
+        myMap = defaultdict(list)
+        myMapIndex = defaultdict(list)
 
-        itr = [iter(g) for g in groups]
+        for i in range(n):
+            key = uf.find(i)
+            myMap[key].append(nums[i])
+            myMapIndex[key].append(i)
+        
+        for key in myMap:
+            myMap[key].sort()
+        
+        ans = [0] * n
 
-        for i in range(len(A)):
-            A[i] = next(itr[gmap[A[i]]])
+        for key in myMap:
+            for i in range(len(myMap[key])):
+                ans[myMapIndex[key][i]] = myMap[key][i]
+            
+        return ans
 
-        return A
+
+
+class UnionFind:
+
+    def __init__(self, n: int):
+        self.n = n
+        self.root = [0]*n
+        for i in range(n):
+            self.root[i] = i
+
+
+    def find(self, x: int) -> int:
+        if self.root[x] == x:
+            return x
+        else:
+            return self.find(self.root[x])
+
+    def union(self, x: int, y: int) -> None:
+        rootX = self.find(x)
+        rootY = self.find(y)
+
+        if rootX != rootY:
+            self.root[rootX] = rootY
+
+    
