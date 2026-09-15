a62ac3ef6fda7117955ac21228b2a28c38758ab5
Aishik Ghosh <174571954+Aishik-Ghosh11@users.noreply.github.com>
2026-09-15 14:23:36 +0530

    Time: 3504 ms (5.15%), Space: 50.8 MB (12.89%) - LeetHub

--- 2472-maximum-number-of-non-overlapping-palindrome-substrings/2472-maximum-number-of-non-overlapping-palindrome-substrings.py
+++ 2472-maximum-number-of-non-overlapping-palindrome-substrings/2472-maximum-number-of-non-overlapping-palindrome-substrings.py
@@ -1,17 +1,34 @@
 class Solution:
     def maxPalindromes(self, s: str, k: int) -> int:
         n = len(s)
-        if k == 1: return n
+        palindromeDp = [[False for _ in range(n)] for j in range(n)]
 
-        res = i = 0
+        for i in range(n):
+            palindromeDp[i][i] = True
+        for length in range(2, n+1):
+            for i in range(n - length + 1):
+                j = i + length - 1
+                if s[i] == s[j]:
+                    if length == 2:
+                        palindromeDp[i][j] = True
+                    else:
+                        palindromeDp[i][j] = palindromeDp[i+1][j-1]
 
-        while i <= n - k:
-            for d in (k, k + 1):
-                if i + d <= n and s[i : i + d] == s[i : i + d][::-1]:
-                    res += 1
-                    i += d
-                    break
-            else:
-                i += 1
+        dp = [0] * (n + 1)
 
-        return res
+        for i in range(n-1, -1, -1):
+
+            dp[i] = dp[i+1]
+            for j in range(i + k - 1, n):
+                if palindromeDp[i][j]:
+                    dp[i] = max(dp[i], 1 + dp[j+1])
+        
+        return dp[0]
+
+
+
+
+
+
+
+
