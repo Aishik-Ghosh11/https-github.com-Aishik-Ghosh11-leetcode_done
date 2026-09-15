12e1876759f0244c6587643f61a03039ded0fd1c
Aishik Ghosh <174571954+Aishik-Ghosh11@users.noreply.github.com>
2026-09-15 20:32:12 +0530

    Time: 296 ms (5.07%), Space: 57.6 MB (15.38%) - LeetHub

--- 2615-sum-of-distances/2615-sum-of-distances.py
+++ 2615-sum-of-distances/2615-sum-of-distances.py
@@ -1,23 +1,15 @@
 class Solution:
     def distance(self, nums: List[int]) -> List[int]:
-        n = len(nums)
-        ans = [0] * n
-
-        mp = defaultdict(list)
-
-        for i, v in enumerate(nums):
-            mp[v].append(i)
+        def helper(nums):
+            ans = [0] * len(nums)
+            sums = collections.defaultdict(int)
+            counts = collections.defaultdict(int)
+            for i, x in enumerate(nums):
+                ans[i] += i * counts[x] - sums[x]
+                sums[x] += i
+                counts[x] += 1
+            return ans
         
-        for pos in mp.values():
-            total = sum(pos)
-            left_sum = 0
-            m = len(pos)
-
-            for i in range(m):
-                right_sum = total - left_sum - pos[i]
-                left = pos[i]*i - left_sum
-                right = right_sum - pos[i] * (m - i - 1)
-
-                ans[pos[i]] = left + right
-                left_sum += pos[i]
-        return ans
+        left = helper(nums)
+        right = helper(nums[::-1])
+        return [x + y for x, y in zip(left , right[::-1])]
