986e6f62b76d829f151824b4bbb019363a5b8255
Aishik Ghosh <174571954+Aishik-Ghosh11@users.noreply.github.com>
2026-09-15 20:18:19 +0530

    Time: 29 ms (7.79%), Space: 67.5 MB (5.14%) - LeetHub

--- 2452-words-within-two-edits-of-dictionary/2452-words-within-two-edits-of-dictionary.cpp
+++ 2452-words-within-two-edits-of-dictionary/2452-words-within-two-edits-of-dictionary.cpp
@@ -1,23 +1,53 @@
 class Solution {
+    struct Node {
+        vector<Node *> next;
+        bool isleaf;
+
+        Node() {
+            next = vector<Node *>(26, nullptr);
+            isleaf = false;
+        }
+    };
 public:
     vector<string> twoEditWords(vector<string>& queries, vector<string>& dictionary) {
-        int n=queries[0].size();
-        vector<string> ans;
-        for(auto &it:queries){
-            for(auto &i:dictionary){
-                int diff=0;
-                for(int idx=0; idx<n; idx++){
-                    if(it[idx]!=i[idx]){
-                        diff++;
-                    }
-                    if(diff>2) break;
-                }
-                if(diff<=2){
-                    ans.push_back(it);
-                    break;
-                }
+        Node *trie = new Node();
+
+        for (string &word : dictionary) {
+            Node *temp = trie;
+            for (char c : word) {
+                int idx = c - 'a';
+
+                if (!temp->next[idx]) temp->next[idx] = new Node();
+                temp = temp->next[idx];
             }
+
+            temp->isleaf = true;
         }
-        return ans;
+
+        int n = queries.size();
+        int i = 0;
+        for (int j = 0; j < n; j++) {
+            queries[i++] = queries[j];
+            if (!cantransform(queries[j], 0, queries[j].size(), trie, 0)) i--;
+        }
+
+        queries.resize(i);
+
+        return queries;
+    }
+
+    bool cantransform(string &s, int i, int n, Node *root, int errors) {
+        if (errors > 2) return false;
+        if (i == n) return root->isleaf;
+
+        int idx = s[i] - 'a';
+        for (int x = 0; x < 26; x++) {
+            if (!root->next[x]) continue;
+
+            bool iserror = (x != idx);
+            if (cantransform(s, i + 1, n, root->next[x], errors + iserror)) return true;
+        }
+
+        return false;
     }
 };
