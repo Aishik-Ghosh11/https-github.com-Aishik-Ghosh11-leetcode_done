f86bb4e6730cab1865dd6b22c494a3d5b296f801
Aishik Ghosh <174571954+Aishik-Ghosh11@users.noreply.github.com>
2026-09-20 15:55:29 +0530

    Time: 271 ms (36.14%), Space: 20.5 MB (35.71%) - LeetHub

--- 1520-maximum-number-of-non-overlapping-substrings/1520-maximum-number-of-non-overlapping-substrings.py
+++ 1520-maximum-number-of-non-overlapping-substrings/1520-maximum-number-of-non-overlapping-substrings.py
@@ -1,34 +1,42 @@
 class Solution:
     def maxNumOfSubstrings(self, s: str) -> list[str]:
-        counts = Counter(s)
-        first = {c: s.find(c) for c in counts}
-        last = {c: s.rfind(c) for c in counts}
-
-        res = []
-        queue = deque()
-
-        for c in counts:
-            queue.appendleft([first[c], last[c], counts[c]])
-
-            left = inf
-            right = -inf
-            total = 0
-
-            for x, y, z in queue:
-                total += z
-                left = min(left, x)
-                right = max(right, y)
-
-                if total == right - left + 1:
-                    break
-
-            if total == right - left + 1:
-                res.append(s[left:right + 1])
-                queue.clear()
-
-        return res
-
-
-
-
+        n = len(s)
+        left = [n] * 26
+        right = [-1] * 26
         
+        
+        for i, ch in enumerate(s):
+            idx = ord(ch) - ord('a')
+            left[idx] = min(left[idx], i)
+            right[idx] = max(right[idx], i)
+            
+        intervals = []
+        for i in range(26):
+            if left[i] == n:
+                continue
+            
+            l, r = left[i], right[i]
+            valid = True
+            j = l
+            while j <= r:
+                char_idx = ord(s[j]) - ord('a')
+                
+                if left[char_idx] < l:
+                    valid = False
+                    break
+        
+                r = max(r, right[char_idx])
+                j += 1
+            if valid:
+                intervals.append([l, r])
+                
+        intervals.sort(key=lambda x: x[1])
+        
+        res = []
+        prev_end = -1
+        for l, r in intervals:
+            if l > prev_end:
+                res.append(s[l:r+1])
+                prev_end = r
+                
+        return res
