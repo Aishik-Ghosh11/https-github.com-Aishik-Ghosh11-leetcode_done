cb8fa8043c7f85b0efdc6b90efbde810a43adcd5
Aishik Ghosh <174571954+Aishik-Ghosh11@users.noreply.github.com>
2026-09-23 13:35:37 +0530

    Time: 13 ms (12.45%), Space: 15 MB (45.44%) - LeetHub

--- 1288-remove-covered-intervals/1288-remove-covered-intervals.cpp
+++ 1288-remove-covered-intervals/1288-remove-covered-intervals.cpp
@@ -1,17 +1,20 @@
 class Solution {
 public:
-    int removeCoveredIntervals(vector<vector<int>>& A) {
-        ranges::sort(A, {}, [](auto& x) {
-            return pair{x[0], -x[1]};
+    int removeCoveredIntervals(vector<vector<int>>& intervals) {
+        sort(intervals.begin(), intervals.end(), [](vector<int>& a, vector<int>& b){
+            if(a[0] == b[0]) return a[1] > b[1];
+            return a[0] < b[0];
         });
-
-        int res = 0, r = 0;
-
-        for (auto& x : A) {
-            res += x[1] > r;
-            r = max(r, x[1]);
+        vector<vector<int>> ans;
+        for(int i = 0; i < intervals.size(); i++){
+            if(i > 0 &&
+               ans.back()[0] <= intervals[i][0] &&
+               ans.back()[1] >= intervals[i][1]){
+                continue;
+            }
+            ans.push_back(intervals[i]);
         }
 
-        return res;
+        return ans.size();
     }
 };
