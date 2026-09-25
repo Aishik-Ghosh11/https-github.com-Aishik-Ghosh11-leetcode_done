c733d36cf114d8ac2a26748bb78da4227bd319fd
Aishik Ghosh <174571954+Aishik-Ghosh11@users.noreply.github.com>
2026-09-25 13:56:28 +0530

    Time: 19 ms (6.01%), Space: 20.1 MB (6.56%) - LeetHub

--- 1096-brace-expansion-ii/1096-brace-expansion-ii.py
+++ 1096-brace-expansion-ii/1096-brace-expansion-ii.py
@@ -1,27 +1,15 @@
 class Solution:
     def braceExpansionII(self, expression: str) -> list[str]:
-        self.i = 0
-        self.expr = expression
-        res = self.parse()
-        return sorted(res)
-    def parse(self):
-        res = set()
-        cur = {""}
-        while self.i < len(self.expr) and self.expr[self.i] != '}':
-            if self.expr[self.i] == '{':
-                self.i += 1
-                nxt = self.parse()
-                self.i += 1
-                cur = self.product(cur, nxt)
-            elif self.expr[self.i] == ',':
-                res |= cur
-                cur = {""}
-                self.i += 1
-            else:
-                nxt = {self.expr[self.i]}
-                self.i += 1
-                cur = self.product(cur, nxt)
-        res |= cur
-        return res
-    def product(self, a, b):
-        return {x + y for x in a for y in b}
+        def dfs(exp):
+            j = exp.find('}')
+            if j == -1:
+                s.add(exp)
+                return 
+            i = exp.rfind('{', 0, j-1)
+            a, c = exp[:i], exp[j + 1 :]
+            for b in exp[i + 1 : j].split(','):
+                dfs(a + b + c)
+        
+        s = set()
+        dfs(expression)
+        return sorted(s)
