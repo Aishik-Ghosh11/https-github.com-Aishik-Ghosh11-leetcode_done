53a3a980227870589f20aaebefb362aa8f1fbbc6
Aishik Ghosh <174571954+Aishik-Ghosh11@users.noreply.github.com>
2026-09-25 21:25:45 +0530

    Time: 1412 ms (5.21%), Space: 512.8 MB (5.07%) - LeetHub

--- 3620-network-recovery-pathways/3620-network-recovery-pathways.cpp
+++ 3620-network-recovery-pathways/3620-network-recovery-pathways.cpp
@@ -1,77 +1,66 @@
 class Solution {
 public:
-    int findMaxPathScore(vector<vector<int>>& edges, vector<bool>& online, long long k) {
-        int n = online.size();
 
-        vector<vector<pair<int,int>>> graph(n);
-        vector<int> indegree(n, 0);
+    bool f(int ans,vector<vector<vector<int>>>&adj,vector<bool>&online,long long k){
+        priority_queue<vector<long long >,vector<vector<long long>>,greater<vector<long long>>>pq;
+        pq.push({0,0,0});
+        int n=online.size();
+        vector<long long >dis(n,1e15);
 
-        for (auto &e : edges) {
-            graph[e[0]].push_back({e[1], e[2]});
-            indegree[e[1]]++;
-        }
+        dis[0]=0;
+        while(!pq.empty()){
+            long long me=pq.top()[1];
+            long long d=pq.top()[0];
+            long long x=pq.top()[2];
+            pq.pop();
+            
+            if(d>dis[x]){continue;}
 
-        queue<int> q;
-        for (int i = 0; i < n; i++)
-            if (indegree[i] == 0)
-                q.push(i);
-
-        vector<int> topo;
-        while (!q.empty()) {
-            int u = q.front();
-            q.pop();
-            topo.push_back(u);
-
-            for (auto &[v, w] : graph[u]) {
-                if (--indegree[v] == 0)
-                    q.push(v);
-            }
-        }
-
-        auto check = [&](int limit) {
-            const long long INF = (1LL << 60);
-
-            vector<long long> dp(n, INF);
-            dp[0] = 0;
-
-            for (int u : topo) {
-
-                if (dp[u] == INF)
-                    continue;
-
-                if (u != 0 && u != n - 1 && !online[u])
-                    continue;
-
-                for (auto &[v, w] : graph[u]) {
-
-                    if (w < limit)
-                        continue;
-
-                    if (v != n - 1 && !online[v])
-                        continue;
-
-                    if (dp[u] + w < dp[v])
-                        dp[v] = dp[u] + w;
+            for(auto it:adj[x]){
+                if((1ll)*(it[1])<ans || online[it[0]]==false){continue;}
+                if(d+(1ll)*(it[1])<dis[it[0]]){
+                    dis[it[0]]=d+it[1];
+                    pq.push({dis[it[0]],max((1ll)*(it[1]),me),it[0]});
                 }
             }
+        }
+        if(dis[n-1]>k){return false;}
+        return true;
+    }
+    int findMaxPathScore(vector<vector<int>>& edges, vector<bool>& online, long long k) {
 
-            return dp[n - 1] <= k;
-        };
-
-        int left = 0, right = 1000000000;
-        int ans = -1;
-
-        while (left <= right) {
-            int mid = left + (right - left) / 2;
-
-            if (check(mid)) {
-                ans = mid;
-                left = mid + 1;
-            } else {
-                right = mid - 1;
-            }
+        if(edges.size()==0){return -1;}
+        int n=online.size();
+        vector<vector<vector<int>>>adj(n);
+        set<int>x1;
+        vector<int>x;
+        for(auto it:edges){
+            int u=it[0];
+            int v=it[1];
+            int c=it[2];
+            adj[u].push_back({v,c});
+            x1.insert(c);
         }
 
-        return ans;
+        for(auto it:x1){
+            x.push_back(it);
+        }
+        int l=0;
+        int r=x.size()-1;
+        if(!f(x[0],adj,online,k)){return -1;}
+        while(r-l>1){
+            int mid=(l+r)/2;
+            if(f(x[mid],adj,online,k)){
+                l=mid;
+            }
+            else{
+                r=mid-1;
+            }
+        }
+        if(f(x[r],adj,online,k)){
+            return x[r];
+        }
+        
+        return x[l];  
     }
 };
