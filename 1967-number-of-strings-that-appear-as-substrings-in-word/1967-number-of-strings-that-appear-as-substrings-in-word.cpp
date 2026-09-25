694760cb202ac9c34525f2e5f8956cfdbb02f284
Aishik Ghosh <174571954+Aishik-Ghosh11@users.noreply.github.com>
2026-09-25 10:19:52 +0530

    Time: 14 ms (10.46%), Space: 38.6 MB (5.39%) - LeetHub

--- 1967-number-of-strings-that-appear-as-substrings-in-word/1967-number-of-strings-that-appear-as-substrings-in-word.cpp
+++ 1967-number-of-strings-that-appear-as-substrings-in-word/1967-number-of-strings-that-appear-as-substrings-in-word.cpp
@@ -1,11 +1,69 @@
 class Solution {
+    struct Node {
+        Node* c[26]{};
+        Node* f{};
+        Node* l{};
+        int n = 0;
+    };
+
+    static constexpr int mx = 10005;
+
 public:
     int numOfStrings(vector<string>& patterns, string word) {
-        int cnt = 0;
+        Node* root = new Node();
 
-        for (const string& s: patterns) {
-            if (word.contains(s))   cnt++;
+        for (auto& p : patterns) {
+            Node* curr = root;
+            for (int i = 0; i < p.size(); i++) {
+                int x = p[i] - 97;
+                if (!curr->c[x]) {             
+                    curr->c[x] = new Node();   
+                }
+                curr = curr->c[x];
+            }
+            curr->n++;
         }
-        return cnt;
+
+        Node* q[mx];
+        int hd = 0;
+        int tl = 0;
+
+        for (int i = 0; i < 26; i++) {
+            if (!root->c[i]) {
+                root->c[i] = root;
+                continue;
+            }
+            root->c[i]->f = root;
+            root->c[i]->l = nullptr;
+            q[tl++] = root->c[i];
+        }
+
+        while (hd < tl) {
+            Node* curr = q[hd++];
+            for (int i = 0; i < 26; i++) {
+                if (!curr->c[i]) {
+                    curr->c[i] = curr->f->c[i];
+                } else {
+                    Node* child = curr->c[i];
+                    child->f = curr->f->c[i];
+                    child->l = (child->f->n > 0) ? child->f : child->f->l;
+                    q[tl++] = child;
+                }
+            }
+        }
+
+        int ans = 0;
+        Node* curr = root;
+        for (char ch : word) {
+            curr = curr->c[ch - 97];
+            for (Node* temp = curr; temp != nullptr; temp = temp->l) {
+                if (temp->n > 0) {
+                    ans += temp->n;
+                    temp->n = 0; 
+                }
+            }
+        }
+
+        return ans;
     }
 };
