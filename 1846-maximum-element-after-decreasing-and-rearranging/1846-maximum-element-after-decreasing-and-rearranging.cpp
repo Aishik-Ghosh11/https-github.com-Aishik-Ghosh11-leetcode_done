b437fb29ce56487a40d1f05e11e7e2f35c63aae0
Aishik Ghosh <174571954+Aishik-Ghosh11@users.noreply.github.com>
2026-09-25 09:40:27 +0530

    Time: 3 ms (90.9%), Space: 55.5 MB (10.22%) - LeetHub

--- 1846-maximum-element-after-decreasing-and-rearranging/1846-maximum-element-after-decreasing-and-rearranging.cpp
+++ 1846-maximum-element-after-decreasing-and-rearranging/1846-maximum-element-after-decreasing-and-rearranging.cpp
@@ -4,10 +4,12 @@ public:
         sort(arr.begin(), arr.end());
         int n = arr.size();
 
-        arr.front() = 1;
-        for (int i=1; i < n; i++) {
-            arr[i] = min(arr[i], arr[i-1] + 1);
+        arr[0] = 1;
+        for (int i=1;i < n; i++) {
+            if (abs(arr[i-1] - arr[i]) > 1) {
+                arr[i] = arr[i - 1] + 1;
+            }
         }
-        return arr.back();
+        return arr[n-1];
     }
 };
